f08f7e717fcefc07ea50cf54ff163799e0a1b34a
cwilliston <carolynnwilliston@gmail.com>
2022-09-28 17:29:12 -0500

    ch 11 exercises

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,17 +7,35 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+        
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0) {
+                throw new ArgumentOutOfRangeException("Cannot divide by zero!");
+            }
+            else { return x / y; }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("fileName", "Must submit a file!");
+            }
+            else
+            {
+                if (fileName.Substring(fileName.Length -3, 3) == ".cs")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
         }
-        */
+        
 
         static void Main(string[] args)
         {
@@ -29,6 +47,16 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+            double num1 = 22;
+            double num2 = 0;
+
+            try
+            {
+                Divide(num1, num2);
+            }
+            catch(ArgumentOutOfRangeException e) { 
+                Console.WriteLine(e.Message);
+                }
 
 
         }
